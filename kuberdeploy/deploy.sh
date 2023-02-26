a626ca20fd98aa824b041f414144d70f9c56dcc8
Ilyas Rufai <rufaiilyas@gmail.com>
2023-02-26 20:04:28 +0100

    feat: working on it...

---
+++ kuberdeploy/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set variables
+RESOURCE_GROUP=aks_rd
+CLUSTER_NAME=aks_clusters
+LOCATION=eastus
+NAMESPACE=kubeflow-dev
+KF_VERSION=v1.3.0
+
+# Create resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Create AKS cluster
+az aks create --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --node-count 2 --enable-addons monitoring --generate-ssh-keys --kubernetes-version 1.24.6 --location $LOCATION
+
+# Connect to cluster
+az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
+
+# Install kubeflow
+kubectl create namespace $NAMESPACE
+kubectl config set-context --current --namespace=$NAMESPACE
+kubectl apply -k "github.com/kubeflow/manifests/kustomize/${KF_VERSION}?ref=${KF_VERSION}"
+
+# Wait for deployment to finish
+kubectl wait --for=condition=available --timeout=10m deployment --all -n $NAMESPACE
+
+# Display Kubeflow endpoint
+echo "Kubeflow endpoint:"
+kubectl describe ingress -n istio-system | grep "Address:"
