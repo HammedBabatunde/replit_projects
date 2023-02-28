8e45ca3d2995077e2ce5408506bc26ba1f9c02b7
Ilyas Rufai <69714197+rufilboy@users.noreply.github.com>
2023-02-28 15:47:47 +0100

    Update deploy.sh

--- kuberdeploy/deploy.sh
+++ kuberdeploy/deploy.sh
@@ -6,7 +6,8 @@ CLUSTER_NAME=aks_cluster
 LOCATION=eastus
 NAMESPACE=kubeflow-dev
 KF_VERSION=v1.3.0
-KUSTOMIZE=/usr/local/bin/kustomize
+KUSTOMIZE_VERSION=$(kustomize version | grep Version | awk '{print $2}')
+# KUSTOMIZE=/usr/local/bin/kustomize
 
 # Create resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
